06dcd7ba0e91433c468e0fec4654022df029be53
omariokartdavis <39391220+omariokartdavis@users.noreply.github.com>
2019-05-08 15:46:08 -0500

    Update movements_list.py

--- metcondatabasev2/metcons/movements_list.py
+++ metcondatabasev2/metcons/movements_list.py
@@ -21,22 +21,29 @@ class MovementList:
         'Overhead Squat': 'Total Body',
         'Thruster': 'Total Body',
         'Wall Ball': 'Total Body',
+        'Burpee': 'Total Body',
+        'Snatch': 'Total Body',
+        'Overhead Squat': 'Total Body',
+        'Burpee Over Bar': 'Total Body',
+        'Bar Facing Burpee': 'Total Body',
         }
 
-##done in python manage.py shell
-##from metcons.models import Classification, Movement, Workout
-##from metcons.movements_list import MovementList
-##
-##all_movements = MovementList()
-##current_movements_in_database = Movement.objects.all()
-##current_movements_in_database_names = [i.name for i in current_movements_in_database]    
-##
-##for k, v in all_movements.movement_list.items():
-##    if k not in current_movements_in_database_names:
-##        movement = Movement(name=k, classification=Classification.objects.get(name=v))
-##        movement.save()
+# to call this function to add movements to the database run:
+# python manage.py runserver
+# exec(open('metcons\movements_list.py').read())
 
+from metcons.models import Classification, Movement, Workout
 
+all_movements = MovementList()
+current_movements_in_database = Movement.objects.all()
+current_movements_in_database_names = [i.name for i in current_movements_in_database]    
+
+for k, v in all_movements.movement_list.items():
+    if k not in current_movements_in_database_names:
+        movement = Movement(name=k, classification=Classification.objects.get(name=v))
+        movement.save()
+
+    
 ##to change this to include abbreviations later:
 ##change movement_list to a list of lists:
 ##movement_list = [
