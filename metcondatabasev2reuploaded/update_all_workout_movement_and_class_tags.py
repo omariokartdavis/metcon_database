67a4c8afd1ba31aa343d1162687efa61fffad8fe
omariokartdavis <39391220+omariokartdavis@users.noreply.github.com>
2019-05-10 14:34:32 -0500

    Upload File

---
+++ metcondatabasev2reuploaded/update_all_workout_movement_and_class_tags.py
@@ -0,0 +1,13 @@
+#run this when you have added a new movement/classification to the database
+# and want to update all current workouts in database with new tags
+
+#to run this:
+#python manage.py shell
+#exec(open('update_all_workout_movement_and_class_tags.py').read())
+
+from metcons.models import Classification, Movement, Workout
+
+all_workouts = Workout.objects.all()
+
+for i in all_workouts:
+    i.update_movements_and_classification()
