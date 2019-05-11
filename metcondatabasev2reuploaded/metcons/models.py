212fb38809f430b45524bb27b3c8319b01469f34
omariokartdavis <39391220+omariokartdavis@users.noreply.github.com>
2019-05-11 18:54:46 -0500

    added 2 models
    added workout instance
    added workout instance completed date
    added created_by_user field to workouts and workout instances

--- metcondatabasev2reuploaded/metcons/models.py
+++ metcondatabasev2reuploaded/metcons/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.urls import reverse
 import datetime
+import uuid
+from django.conf import settings
+from django.db.models import Count
 
 class Classification(models.Model):
         """Model representing a classification of a movement"""
@@ -45,17 +48,18 @@ class Workout(models.Model):
         #each workout needs an instance model in order to track number of times completed for each user
         date_created = models.DateField(default=datetime.date.today)
         date_added_to_database = models.DateField(auto_now_add = True)
-        date_completed = models.DateField(auto_now=True)
         number_of_times_completed = models.IntegerField(default=0)
         workout_text = models.TextField(max_length=2000)
         scaling_or_description_text = models.TextField(max_length=4000, null=True, blank=True)
         what_website_workout_came_from = models.CharField(max_length=200, null=True, blank=True)
         estimated_duration_in_minutes = models.IntegerField(default=0, null=True, blank=True)
+        created_by_user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
                 
         movements = models.ManyToManyField(Movement, blank=True)
 
-        classification = models.ForeignKey(Classification, default=3, blank=True, null=True, on_delete=models.CASCADE) #had to run makemigrations and migrate after making the blank=true null=True change to allow for no classifications when creating a workout model
+        classification = models.ForeignKey(Classification, default=3, blank=True, null=True, on_delete=models.CASCADE)
 
+        
         #Required for running both functions on click in html
         #next step is to transfer this to javascript so you can also force page reload at the same time
         def update_movements_and_classification(self):
@@ -90,9 +94,15 @@ class Workout(models.Model):
             else:
                 self.classification = None
 
-        def update_times_completed_counter(self):
+        def increment_times_completed_counter(self):
+                #not sure if this is the proper way to do this
                 self.number_of_times_completed += 1
+                self.save()
             
+        def number_of_instances(self):
+                count = WorkoutInstance.objects.filter(workout__id = self.id).count()
+                return count
+        
         class Meta:
                 ordering = ['-number_of_times_completed', '-date_created', '-id']
 
@@ -121,4 +131,60 @@ class Workout(models.Model):
         def __str__(self):
                 """String for representing the model object."""
                 name = "Workout " + str(self.id)
+                return name
+        
+class WorkoutInstanceCompletedDate(models.Model):
+        """Dates of when workout instances are completed"""
+        date_completed = models.DateField(default=datetime.date.today)
+
+        class Meta:
+                ordering = ['-date_completed']
+
+        def __str__(self):
+                name = str(self.date_completed)
+                return name
+        
+class WorkoutInstance(models.Model):
+        """Model representing a specific Users Workout Instance"""
+        id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text = 'Unique ID for this particular workout')
+        dates_workout_completed = models.ManyToManyField(WorkoutInstanceCompletedDate, blank=True)
+        date_added_by_user = models.DateField(auto_now_add=True)
+        workout = models.ForeignKey(Workout, on_delete=models.SET_NULL, null=True)
+        number_of_times_completed = models.IntegerField(default=0)
+        duration_in_minutes = models.IntegerField(default=0, null=True, blank=True)
+        current_user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True)
+
+        current_user.short_description = 'User'
+        
+        class Meta:
+                ordering = ['-number_of_times_completed', '-date_added_by_user', '-id']
+                
+        def add_date_completed(self, date):
+                #need to work on this as not currently sure what type I want date input to be. should it be a string or datetime object? datetime object likely easier but how do I get it into
+                # that form?
+                all_current_dates = WorkoutInstanceCompletedDate.objects.all()
+                if date in all_current_dates:
+                        self.dates_workout_completed.add(date)
+                else:
+                        new_date = WorkoutInstanceCompletedDate(date_completed = date)
+                        self.dates_workout_completed.add(new_date)
+                        
+        def increment_times_completed(self):
+                #not sure if this works or is proper
+                self.number_of_times_completed += 1
+                self.save()
+                self.workout.increment_times_completed_counter()
+                
+        def display_name(self):
+                name = self.workout.display_name()
+                return name
+
+        def display_dates_completed(self):
+                """Create a string for the classification. required to display classificaitons in admin site"""
+                return ', '.join(str(date) for date in self.dates_workout_completed.all()[:3])
+
+        display_dates_completed.short_description = 'Dates Completed'
+        
+        def __str__(self):
+                name = self.workout.display_name()
                 return name
