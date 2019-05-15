a0591d564e483855c645374a54a1cf63bdbc13cc
omariokartdavis <39391220+omariokartdavis@users.noreply.github.com>
2019-05-14 22:05:05 -0500

    added accounts path

--- metcondatabasev2reuploaded/metcondatabasev2/urls.py
+++ metcondatabasev2reuploaded/metcondatabasev2/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-	path('metcons/', include('metcons.urls')),
-	path('', RedirectView.as_view(url='/metcons/', permanent=True)),
+    path('metcons/', include('metcons.urls')),
+    path('', RedirectView.as_view(url='/metcons/', permanent=True)),
+    path('accounts/', include('django.contrib.auth.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
