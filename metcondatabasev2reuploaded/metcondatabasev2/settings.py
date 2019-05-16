9f2fd6fa0e0044382368f425b904d8a4b42b00a1
omariokartdavis <39391220+omariokartdavis@users.noreply.github.com>
2019-05-15 23:32:35 -0500

    login and logout redirect

--- metcondatabasev2reuploaded/metcondatabasev2/settings.py
+++ metcondatabasev2reuploaded/metcondatabasev2/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -123,3 +124,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+LOGIN_REDIRECT_URL = '/metcons/profile/'
+
+LOGOUT_REDIRECT_URL = '/'
