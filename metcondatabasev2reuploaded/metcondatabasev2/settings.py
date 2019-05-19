e1e8e7d1f59299914adb9d3a69eca0932bc963c2
omariokartdavis <39391220+omariokartdavis@users.noreply.github.com>
2019-05-18 23:12:17 -0500

    profile redirect on login

--- metcondatabasev2reuploaded/metcondatabasev2/settings.py
+++ metcondatabasev2reuploaded/metcondatabasev2/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-
+from django.urls import reverse
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -125,6 +125,6 @@ STATIC_URL = '/static/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-LOGIN_REDIRECT_URL = '/metcons/profile/'
+LOGIN_REDIRECT_URL = '/metcons/profileredirect/'
 
 LOGOUT_REDIRECT_URL = '/'
