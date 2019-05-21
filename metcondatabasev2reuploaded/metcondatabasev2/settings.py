57b25a0a74897e2b3b34256563dcadd73de74460
omariokartdavis <39391220+omariokartdavis@users.noreply.github.com>
2019-05-21 14:48:33 -0500

    mediaurl and root

--- metcondatabasev2reuploaded/metcondatabasev2/settings.py
+++ metcondatabasev2reuploaded/metcondatabasev2/settings.py
@@ -66,6 +66,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -122,6 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
