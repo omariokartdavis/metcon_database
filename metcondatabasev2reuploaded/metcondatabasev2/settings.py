9982f0689a1c9cf293816611a0c727bd1bd61958
omariokartdavis <39391220+omariokartdavis@users.noreply.github.com>
2019-05-30 15:01:09 -0500

    new user model

--- metcondatabasev2reuploaded/metcondatabasev2/settings.py
+++ metcondatabasev2reuploaded/metcondatabasev2/settings.py
@@ -89,6 +89,7 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 
+AUTH_USER_MODEL = 'metcons.User'
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
