f1b3291f9e76c22afbd64a46661792cb9de3384a
omariokartdavis <39391220+omariokartdavis@users.noreply.github.com>
2019-06-19 19:00:27 -0500

    signup, add athlete, add coach

--- metcondatabasev2reuploaded/metcons/urls.py
+++ metcondatabasev2reuploaded/metcons/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('profileredirect/', views.profileredirect),
     path('', views.index, name='index'),
+    path('signup/', views.signup, name='signup'),
     path('workouts/', views.workoutlistview, name='workouts'),
     path('workout/<int:pk>', views.workoutdetailview, name='workout-detail'),
     path('movements/', views.MovementListView.as_view(), name='movements'),
@@ -21,4 +22,6 @@ urlpatterns = [
     path('<username>/workout/<uuid:pk>/result/<int:resultid>/edit/', views.edit_result, name='edit_result'),
     path('<username>/workout/<uuid:pk>/result/<int:resultid>/delete/', views.delete_result, name='delete_result'),
     path('<username>/workout/<uuid:pk>/schedule or add result/', views.schedule_recently_created_or_added_workout, name='interim_created_workout'),
+    path('<username>/add athlete/', views.add_athletes_to_coach, name='add_athletes'),
+    path('<username>/add coach/', views.add_coach, name='add_coach'),
 ]
