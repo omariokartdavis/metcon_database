a6c8e068c2475cf93ba63f53476eee1999a02a2a
omariokartdavis <39391220+omariokartdavis@users.noreply.github.com>
2019-07-09 22:42:38 -0500

    added dropdown in sidebar

--- metcondatabasev2reuploaded/metcons/static/js/base_generic.js
+++ metcondatabasev2reuploaded/metcons/static/js/base_generic.js
@@ -1,9 +1,24 @@
+var dropdown = document.getElementsByClassName("dropdownOpener");
+
+function dropdownFunction() {
+	this.classList.toggle("active");
+	var dropdownContent = this.nextElementSibling;
+	if (dropdownContent.style.height == "60%") {
+		dropdownContent.style.height = "0";
+	} else {
+		dropdownContent.style.height = "60%";
+	}
+};
+
 function openNav() {
 	document.getElementsByClassName("sidebar-nav")[0].style.width = "300px";
 	document.getElementsByClassName("sidebar-nav")[0].style.left = "0";
 	//document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 	document.getElementsByClassName("backgroundDim")[0].style.visibility = "visible";
 	document.getElementsByClassName("backgroundDim")[0].style.opacity = "0.4";
+	for (var i = 0; i < dropdown.length; i++) {
+		dropdown[i].addEventListener("click", dropdownFunction, false) //this for loop has to be inside the openNav() function because it can't apply the on click event before the side nav has been opened. The buttons don't exist yet.
+	}
 }
 
 function closeNav() {
