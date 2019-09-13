281036e1cb3612da050d7c130dade8d9e0700b3a
omariokartdavis <39391220+omariokartdavis@users.noreply.github.com>
2019-09-13 14:46:31 -0500

    moved workoutdropdownbuttons to base_generic.js

--- metcondatabasev2reuploaded/metcons/static/js/user_page.js
+++ metcondatabasev2reuploaded/metcons/static/js/user_page.js
@@ -1,24 +1,4 @@
-var workoutDropdowns = document.getElementsByClassName("dropdownOpenerWorkoutButtons");
 
-function workoutDropdownFunction() {
-	this.classList.toggle("active");
-	var dropdownContent = this.nextElementSibling;
-	if (dropdownContent.style.height == "auto") {
-		dropdownContent.style.height = "0";
-		dropdownContent.style.width = "0";
-		dropdownContent.style.boxShadow = "none";
-	} else {
-		dropdownContent.style.height = "auto";
-		dropdownContent.style.width = "auto";
-		dropdownContent.style.boxShadow = "0 0 0 1px rgba(0,0,0,.15), 0 2px 3px rgba(0,0,0,.2)";
-	}
-};
-
-var i;
-for (var i = 0; i < workoutDropdowns.length; i++) {
-	workoutDropdowns[i].addEventListener("click", workoutDropdownFunction, false)
-}
-	
 function openTab(evt) {
   // Declare all variables
   var i, tabcontent, tablinks, tabName;
